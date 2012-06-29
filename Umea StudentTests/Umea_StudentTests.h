b76184759abfb738641aa46808bca84ef8a97c96
Joakim Mickelsson <joakim.mickelsson@gmail.com>
2012-06-30 01:42:05 +0200

    Initial Commit

---
+++ Umea StudentTests/Umea_StudentTests.h
@@ -0,0 +1,13 @@
+//
+//  Umea_StudentTests.h
+//  Umea StudentTests
+//
+//  Created by Joakim Mickelsson on 6/30/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <SenTestingKit/SenTestingKit.h>
+
+@interface Umea_StudentTests : SenTestCase
+
+@end
---
+++ Umea StudentTests/Umea_StudentTests.m
@@ -0,0 +1,32 @@
+//
+//  Umea_StudentTests.m
+//  Umea StudentTests
+//
+//  Created by Joakim Mickelsson on 6/30/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import "Umea_StudentTests.h"
+
+@implementation Umea_StudentTests
+
+- (void)setUp
+{
+    [super setUp];
+    
+    // Set-up code here.
+}
+
+- (void)tearDown
+{
+    // Tear-down code here.
+    
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    STFail(@"Unit tests are not implemented yet in Umea StudentTests");
+}
+
+@end
